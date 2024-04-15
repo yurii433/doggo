6fd538be7915ea7a5c5a833c86f1c31540099e20
yurii433 <yuradiachuk1@gmail.com>
2024-04-15 19:08:09 +0200

    search params bug fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,7 +9,7 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Suspense } from "react";
 
-const Blog = ({}) => {
+const BlogContent = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -52,83 +52,88 @@ const Blog = ({}) => {
     borderBottom: "2px solid black",
     fontWeight: "800",
   };
-
   return (
-    <Suspense>
-      <div className={styles.blog}>
-        <div className={styles.container}>
-          <div className={styles.blogTop}>
-            <h1>
-              The Dog <i className={arizonia.className}> Blog</i>
-            </h1>
+    <div className={styles.container}>
+      <div className={styles.blogTop}>
+        <h1>
+          The Dog <i className={arizonia.className}> Blog</i>
+        </h1>
 
-            <nav className={styles.filterNav}>
-              <button
-                style={activeButton === "all" ? activeButtonStyle : {}}
-                onClick={() => handleFiltering("")}
-              >
-                All
-              </button>
-              <button
-                onClick={() => handleFiltering("nutrition")}
-                style={activeButton === "nutrition" ? activeButtonStyle : {}}
-              >
-                Nutrition
-              </button>
-              <button
-                onClick={() => handleFiltering("supplements")}
-                style={activeButton === "supplements" ? activeButtonStyle : {}}
-              >
-                Supplements
-              </button>
-              <button
-                onClick={() => handleFiltering("lifestyle")}
-                style={activeButton === "lifestyle" ? activeButtonStyle : {}}
-              >
-                Lifestyle
-              </button>
-            </nav>
-          </div>
-          {isLoading ? (
-            <h2>Blog posts are loading...</h2>
+        <nav className={styles.filterNav}>
+          <button
+            style={activeButton === "all" ? activeButtonStyle : {}}
+            onClick={() => handleFiltering("")}
+          >
+            All
+          </button>
+          <button
+            onClick={() => handleFiltering("nutrition")}
+            style={activeButton === "nutrition" ? activeButtonStyle : {}}
+          >
+            Nutrition
+          </button>
+          <button
+            onClick={() => handleFiltering("supplements")}
+            style={activeButton === "supplements" ? activeButtonStyle : {}}
+          >
+            Supplements
+          </button>
+          <button
+            onClick={() => handleFiltering("lifestyle")}
+            style={activeButton === "lifestyle" ? activeButtonStyle : {}}
+          >
+            Lifestyle
+          </button>
+        </nav>
+      </div>
+      {isLoading ? (
+        <h2>Blog posts are loading...</h2>
+      ) : (
+        <div className={styles.blogBody}>
+          {filteredBlogData.length > 0 ? (
+            filteredBlogData.map((post) => {
+              return (
+                <Link
+                  href={`/blog/${post.postId}`}
+                  className={styles.blogPost}
+                  key={post.postId}
+                >
+                  <Image
+                    width={400}
+                    height={400}
+                    src={post.postImage.src}
+                    alt={post.postImage.alt}
+                    className={styles.postImg}
+                    loading="lazy"
+                  />
+                  <div className={styles.postInfo}>
+                    <i className={arizonia.className}>
+                      {" "}
+                      {post.postInfo.category[0].toUpperCase() +
+                        post.postInfo.category.substring(1)}
+                    </i>
+                    <div className={styles.vLine}></div>
+                    <p>{post.postInfo.time}</p>
+                    <FaRegClock />
+                  </div>
+                  <h3 className={styles.postHeader}>{post.postTitle}</h3>
+                </Link>
+              );
+            })
           ) : (
-            <div className={styles.blogBody}>
-              {filteredBlogData.length > 0 ? (
-                filteredBlogData.map((post) => {
-                  return (
-                    <Link
-                      href={`/blog/${post.postId}`}
-                      className={styles.blogPost}
-                      key={post.postId}
-                    >
-                      <Image
-                        width={400}
-                        height={400}
-                        src={post.postImage.src}
-                        alt={post.postImage.alt}
-                        className={styles.postImg}
-                        loading="lazy"
-                      />
-                      <div className={styles.postInfo}>
-                        <i className={arizonia.className}>
-                          {" "}
-                          {post.postInfo.category[0].toUpperCase() +
-                            post.postInfo.category.substring(1)}
-                        </i>
-                        <div className={styles.vLine}></div>
-                        <p>{post.postInfo.time}</p>
-                        <FaRegClock />
-                      </div>
-                      <h3 className={styles.postHeader}>{post.postTitle}</h3>
-                    </Link>
-                  );
-                })
-              ) : (
-                <h2>Sorry, blog posts are not availiable</h2>
-              )}
-            </div>
+            <h2>Sorry, blog posts are not availiable</h2>
           )}
         </div>
+      )}
+    </div>
+  );
+};
+
+const Blog = ({}) => {
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <div className={styles.blog}>
+        <BlogContent />
       </div>
     </Suspense>
   );
