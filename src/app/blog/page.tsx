c4de5d1a3e69b0de0cee99f0014d24dc8048dd1a
yurii433 <yuradiachuk1@gmail.com>
2024-04-15 15:37:31 +0200

    build bug fixes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import styles from "./page.module.css";
 import { arizonia } from "@/app/utils/fonts";
 import { FaRegClock } from "react-icons/fa";
@@ -7,9 +6,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { blogData } from "../utils/blogData";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { NextRequest } from "next/server";
 import { useEffect, useState } from "react";
-import { query } from "firebase/firestore";
+import { Suspense } from "react";
 
 const Blog = ({}) => {
   const searchParams = useSearchParams();
@@ -56,81 +54,83 @@ const Blog = ({}) => {
   };
 
   return (
-    <div className={styles.blog}>
-      <div className={styles.container}>
-        <div className={styles.blogTop}>
-          <h1>
-            The Dog <i className={arizonia.className}> Blog</i>
-          </h1>
+    <Suspense>
+      <div className={styles.blog}>
+        <div className={styles.container}>
+          <div className={styles.blogTop}>
+            <h1>
+              The Dog <i className={arizonia.className}> Blog</i>
+            </h1>
 
-          <nav className={styles.filterNav}>
-            <button
-              style={activeButton === "all" ? activeButtonStyle : {}}
-              onClick={() => handleFiltering("")}
-            >
-              All
-            </button>
-            <button
-              onClick={() => handleFiltering("nutrition")}
-              style={activeButton === "nutrition" ? activeButtonStyle : {}}
-            >
-              Nutrition
-            </button>
-            <button
-              onClick={() => handleFiltering("supplements")}
-              style={activeButton === "supplements" ? activeButtonStyle : {}}
-            >
-              Supplements
-            </button>
-            <button
-              onClick={() => handleFiltering("lifestyle")}
-              style={activeButton === "lifestyle" ? activeButtonStyle : {}}
-            >
-              Lifestyle
-            </button>
-          </nav>
-        </div>
-        {isLoading ? (
-          <h2>Blog posts are loading...</h2>
-        ) : (
-          <div className={styles.blogBody}>
-            {filteredBlogData.length > 0 ? (
-              filteredBlogData.map((post) => {
-                return (
-                  <Link
-                    href={`/blog/${post.postId}`}
-                    className={styles.blogPost}
-                    key={post.postId}
-                  >
-                    <Image
-                      width={400}
-                      height={400}
-                      src={post.postImage.src}
-                      alt={post.postImage.alt}
-                      className={styles.postImg}
-                      loading="lazy"
-                    />
-                    <div className={styles.postInfo}>
-                      <i className={arizonia.className}>
-                        {" "}
-                        {post.postInfo.category[0].toUpperCase() +
-                          post.postInfo.category.substring(1)}
-                      </i>
-                      <div className={styles.vLine}></div>
-                      <p>{post.postInfo.time}</p>
-                      <FaRegClock />
-                    </div>
-                    <h3 className={styles.postHeader}>{post.postTitle}</h3>
-                  </Link>
-                );
-              })
-            ) : (
-              <h2>Sorry, blog posts are not availiable</h2>
-            )}
+            <nav className={styles.filterNav}>
+              <button
+                style={activeButton === "all" ? activeButtonStyle : {}}
+                onClick={() => handleFiltering("")}
+              >
+                All
+              </button>
+              <button
+                onClick={() => handleFiltering("nutrition")}
+                style={activeButton === "nutrition" ? activeButtonStyle : {}}
+              >
+                Nutrition
+              </button>
+              <button
+                onClick={() => handleFiltering("supplements")}
+                style={activeButton === "supplements" ? activeButtonStyle : {}}
+              >
+                Supplements
+              </button>
+              <button
+                onClick={() => handleFiltering("lifestyle")}
+                style={activeButton === "lifestyle" ? activeButtonStyle : {}}
+              >
+                Lifestyle
+              </button>
+            </nav>
           </div>
-        )}
+          {isLoading ? (
+            <h2>Blog posts are loading...</h2>
+          ) : (
+            <div className={styles.blogBody}>
+              {filteredBlogData.length > 0 ? (
+                filteredBlogData.map((post) => {
+                  return (
+                    <Link
+                      href={`/blog/${post.postId}`}
+                      className={styles.blogPost}
+                      key={post.postId}
+                    >
+                      <Image
+                        width={400}
+                        height={400}
+                        src={post.postImage.src}
+                        alt={post.postImage.alt}
+                        className={styles.postImg}
+                        loading="lazy"
+                      />
+                      <div className={styles.postInfo}>
+                        <i className={arizonia.className}>
+                          {" "}
+                          {post.postInfo.category[0].toUpperCase() +
+                            post.postInfo.category.substring(1)}
+                        </i>
+                        <div className={styles.vLine}></div>
+                        <p>{post.postInfo.time}</p>
+                        <FaRegClock />
+                      </div>
+                      <h3 className={styles.postHeader}>{post.postTitle}</h3>
+                    </Link>
+                  );
+                })
+              ) : (
+                <h2>Sorry, blog posts are not availiable</h2>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
